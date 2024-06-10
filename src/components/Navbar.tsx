56b370d1291a4d7c4971d1dfd33df023e3ad89cf
Ravish Bisht <ravishbisht85@gmail.com>
2024-06-10 06:56:15 +0530

    img and navbar add

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="bg-primary dark:bg-slate-700 py-2 px-3 flex justify-between text-white">
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
