2d0d3e42e189377ec56885b52a8fec9981985d9a
Ravish Bisht <ravishbisht85@gmail.com>
2024-06-10 07:32:28 +0530

    add dropdown-menu when click on avatar inside navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,14 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from "../../img/logo.png";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
   return (
@@ -10,8 +18,22 @@ const Navbar = () => {
         <Image src={logo} height={40} alt="TraversyPress" />
       </Link>
       <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-        <AvatarFallback className="text-black">CN</AvatarFallback>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="focus:outline-none">
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback className="text-black">CN</AvatarFallback>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/profile">Logout</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/auth">Profile</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </Avatar>
     </div>
   );
