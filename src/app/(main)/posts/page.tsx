cd99250468312cf6583a467924953f5d0d2e4efd
Ravish Bisht <ravishbisht85@gmail.com>
2024-06-10 15:27:37 +0530

    add back button

--- src/app/(main)/posts/page.tsx
+++ src/app/(main)/posts/page.tsx
@@ -1,9 +1,13 @@
+import BackButton from "@/components/BackButton";
 import PostsTable from "@/components/posts/PostsTable";
 
 const PostPage = () => {
-  return <>
- <PostsTable/> 
-  </>;
+  return (
+    <>
+      <BackButton text="go Back" link="/" />
+      <PostsTable />
+    </>
+  );
 };
 
 export default PostPage;
