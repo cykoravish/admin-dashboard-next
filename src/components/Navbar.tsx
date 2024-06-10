c623e50f5c0757e2207afa235122986f5b43a68e
Ravish Bisht <ravishbisht85@gmail.com>
2024-06-10 07:18:45 +0530

    add items in navbar and styled it

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../img/logo.png";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 const Navbar = () => {
   return (
     <div className="bg-primary dark:bg-slate-700 py-2 px-3 flex justify-between text-white">
-      Navbar
+      <Link href="/">
+        <Image src={logo} height={40} alt="TraversyPress" />
+      </Link>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback className="text-black">CN</AvatarFallback>
+      </Avatar>
     </div>
   );
 };
